9296e70f6dbc56ef4c71ae102bb78baae1e99933
David Adamson <david@212digital.co.uk>
2021-05-04 13:22:02 +0100

    Add project files.

---
+++ BlueMoonWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlueMoonWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
